b947a29622b91f576dc23ac14f9f191d29efe134
Jxshyy592 <josh-sloan@hotmail.co.uk>
2024-10-22 23:24:34 +0100

    fap

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, make_response, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return make_response("<h1>Hello World</h1>", 200)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ convertCSV.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+with open ("DisneylandReviews.csv", "r") as f:
+    reader = csv.reader(f)
+    next(reader)
+    data = []
+    for row in reader:
+        data.append({"ID": row[0],
+                              "Rating": row[1],
+                              "Year_Month": row[2],
+                              "Reviewer Location": row[3],
+                              "Review": row[4],
+                              "Branch": row[5],
+                              })
+
+with open("disneyReviews.json", "w") as f:
+    json.dump(data, f, indent=4)
